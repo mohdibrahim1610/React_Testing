d5582e6fc96b21a6106ab144c6e9c948e5e9a212
mohdibrahim1610 <53181422+mohdibrahim1610@users.noreply.github.com>
2024-08-27 15:11:41 +0000

    Navbar adding links init

---
+++ react-router-demo/src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+ const Navbar = () => {
+  return (
+    
+    <nav >
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About US</Link>
+
+    </nav>
+
+  )
+}
+
+export default Navbar;
